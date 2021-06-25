ee3c9971de15d06f3e438ce798d4ffff82a143bf
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-06-25 12:59:39 +0600

    html-ejs-setup

---
+++ router/loginRouter.js
@@ -0,0 +1,15 @@
+// external import
+const express = require("express");
+
+
+// internal imports
+const {getLogin} = require("../controller/loginController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+
+const router = express.Router();
+
+//login page
+router.get("/", decorateHtmlResponse("Login"), getLogin);
+
+
+module.exports = router;
---
+++ router/usersRouter.js
@@ -0,0 +1,15 @@
+// external import
+const express = require("express");
+
+
+// internal imports
+const {getUsers} = require("../controller/usersController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+
+const router = express.Router();
+
+//users page
+router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+
+module.exports = router;
