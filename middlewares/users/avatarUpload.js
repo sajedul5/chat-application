eb28d5e2cfd98521476ac637728f4a063e5dc20b
greeho951 <greeho951@gmail.com>
2021-07-02 16:24:26 +0600

    file upload

--- middlewares/users/avatarUpload.js
+++ middlewares/users/avatarUpload.js
@@ -1,3 +1,5 @@
+const uploader = require("../../utilities/singleUploader");
+
 function avatarUpload(req, res, next){
 
     const upload = uploader(
@@ -6,6 +8,21 @@ function avatarUpload(req, res, next){
         1000000,
         "Only .jpg, .jpeg or .png format allowed!"
     );
+
+    // call the middleware function
+    upload.any()(req, res, (err) => {
+        if(err){
+            res.status(500).json({
+                errors: {
+                    avatar: {
+                        msg: err.message,
+                    },
+                },
+            });
+        }else{
+            next();
+        }
+    });
 
 }
 
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -5,11 +5,15 @@ const express = require("express");
 // internal imports
 const {getUsers} = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
 
 const router = express.Router();
 
 //users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post("/", avatarUpload);
 
 
 module.exports = router;
